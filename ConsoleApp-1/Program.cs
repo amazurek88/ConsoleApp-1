eaeb4c678dde05ec7752f6c923caaf70978277df
jacek <ola88mazurek@gmail.com>
2021-08-31 20:41:37 +0200

    application - comparison of numbers

---
+++ ConsoleApp-1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lista = new List<int>();
+            
+            lista.Add(int.Parse(Console.ReadLine()));
+            lista.Add(int.Parse(Console.ReadLine()));
+
+            if (lista[0] > lista[1])
+            {
+                Console.WriteLine("Liczba 1 jest większa od liczby 2.");
+            }
+
+            if (lista[0] < lista[1])
+            {
+                Console.WriteLine("Liczba 1 jest mniejsza od liczby 2.");
+            }
+
+            if (lista[0] == lista[1])
+            {
+                Console.WriteLine("Liczba 1 jest równa liczbie 2.");
+            }
+        }
+    }
+}
